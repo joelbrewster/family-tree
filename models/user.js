1aa0f5f87ed812c87b35ebe94a1310681f707fb1
Joel Brewster <j.n.brewster@gmail.com>
2016-06-23 14:41:24 +1000

    pre hook function

--- models/user.js
+++ models/user.js
@@ -20,6 +20,15 @@ var userSchema = new mongoose.Schema({
 userSchema.methods.sayHello = function(){
   console.log('Hi ' + this.first_name);
 }
+//define a pre save hook function
+userSchema.pre('save', function(next){
+  var now = new Date();
+  this.updated_at = now;
+  if(!this.created_at){
+    this.created_at = new;
+  }
+  next();
+});
 
 //registering our schema with mongoose / mongo
 var User = mongoose.model('User', userSchema);
